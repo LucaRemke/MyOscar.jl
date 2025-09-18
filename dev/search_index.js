7fa42a07e7ac73048b6baac2b9df883978f09632
Documenter.jl <documenter@juliadocs.github.io>
2025-09-18 07:42:26 +0000

    build based on 8398f04

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyOscar","category":"page"},{"location":"#MyOscar","page":"Home","title":"MyOscar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyOscar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyOscar]","category":"page"}]
+[{"location":"#MyOscar","page":"Home","title":"MyOscar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyOscar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
